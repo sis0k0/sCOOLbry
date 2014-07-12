0611806528dc20dd4fbb9a6688c7ff5295a15f0d
sis0k0 <sis0k0@abv.bg>
2014-07-12 21:36:29 +0300

    html5Mode
    html5Mode (no # in the links) done

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('app', ['ngResource', 'ngRoute']).value('toastr', toastr);
 
 app.config(function($routeProvider, $locationProvider) {
-    // $locationProvider.html5Mode(true);
+     $locationProvider.html5Mode(true);
 
     var routeUserChecks = {
         adminRole: {
