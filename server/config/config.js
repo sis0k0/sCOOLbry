d50350a62d252938afd5a14530238ff61aab6dda
sis0k0 <sis0k0@abv.bg>
2014-07-15 11:33:59 +0300

    Config for production

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ var rootPath = path.normalize(__dirname + '/../../')
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost/telerikacademycourses',
+        db: 'mongodb://localhost/scoolbry',
         port: process.env.PORT || 3030
     },
     production: {
