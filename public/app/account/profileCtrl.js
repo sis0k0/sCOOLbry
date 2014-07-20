fb2da7f53916a3bb200a31eb74ca7fbd70a2cec8
peshopbs2 <peshopbs2@gmail.com>
2014-07-20 03:35:35 +0300

    Update profileCtrl.js
    Added avatar, facebook url, twitter url, googleplus url, favourite book, about us

--- public/app/account/profileCtrl.js
+++ public/app/account/profileCtrl.js
@@ -4,7 +4,12 @@ app.controller('ProfileCtrl', function($scope, $location, auth, identity) {
         firstName: identity.currentUser.firstName,
         lastName: identity.currentUser.lastName,
         roles: identity.currentUser.roles,
-        avatar: identity.currentUser.avatar
+        avatar: identity.currentUser.avatar,
+        fb_url: identity.currentUser.fb_url,
+        twitter_url: identity.currentUser.twitter_url,
+        googleplus_url: identity.currentUser.googleplus_url,
+        favourite_book: identity.currentUser.favourite_book,
+        about_us: identity.currentUser.about_us
     }
 
     $scope.update = function(user) {
@@ -13,6 +18,12 @@ app.controller('ProfileCtrl', function($scope, $location, auth, identity) {
             $scope.lastName = user.lastName;
             $scope.roles = user.roles;
             $scope.avatar = user.avatar;
+            $scope.fb_url = user.fb_url;
+            $scope.twitter_url = user.twitter_url;
+            $scope.googleplus_url = user.googleplus_url;
+            $scope.favourite_book = user.favourite_book;
+            $scope.about_us = user.about_us;
+        
             $location.path('/');
         });
     }
