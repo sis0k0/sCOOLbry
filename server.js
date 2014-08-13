4001c307adbbd3ee626a977a161c7a3755b189bb
sis0k0 <sis0k0@abv.bg>
2014-08-13 17:28:03 +0300

    Changed to production

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'production';
 
 var app = express();
 var config = require('./server/config/config')[env];
