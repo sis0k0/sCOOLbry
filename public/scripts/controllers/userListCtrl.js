00ad0b3ae786163cfbd1c915e1bcc66ed0c15a88
sis0k0 <sis0k0@abv.bg>
2014-08-15 20:05:44 +0300

    Some admin panel user list
    - filter
    - load more/all

--- public/scripts/controllers/userListCtrl.js
+++ public/scripts/controllers/userListCtrl.js
@@ -1,3 +1,19 @@
 app.controller('UserListCtrl', function($scope, UsersResource) {
     $scope.users = UsersResource.query();
+
+    
+    // infinite scroller
+
+    $scope.limitStep = 5;
+
+    $scope.totalDisplayed = $scope.limitStep;
+
+	$scope.loadMore = function () {
+	  $scope.totalDisplayed += $scope.limitStep; 
+	};
+
+	$scope.loadAll = function() {
+		$scope.totalDisplayed = $scope.users.length;
+	}
+
 });
