f994a79eebd3b5ebcf37e280fe4e01b46810952e
sis0k0 <sis0k0@abv.bg>
2014-08-19 12:25:56 +0300

    Fixed invalid imaged and added error. Added library

---
+++ public/scripts/services/cachedLibraries.js
@@ -0,0 +1,13 @@
+app.factory('cachedLibraries', function(LibraryResource) {
+    var cachedLibraries;
+
+    return {
+        query: function() {
+            if (!cachedLibraries) {
+                cachedLibraries = LibraryResource.query();
+            }
+
+            return cachedLibraries;
+        }
+    }
+});
---
+++ server/models/Library.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose');
+
+var librarySchema = mongoose.Schema({
+    name: String,
+    address: String,
+    featured: Boolean,
+    description: String,
+    published: Date,
+    ownerId: String,
+    tags: [String]
+});
+
+var Library = mongoose.model('Library', librarySchema);
+
+module.exports.seedInitialLibraries = function() {
+    Library.find({}).exec(function(err, collection) {
+        if (err) {
+            console.log('Cannot find Librarys: ' + err);
+            return;
+		}
+		
+        if (collection.length === 0) {
+            Library.create({name: 'ПМГ Бургас', featured: true, published: new Date('19/8/2014'), tags: ['ученическа библиотека, българска литература, световна литература']});
+            Library.create({name: 'Библиотека на ТКЗС в с. Библиотекарово', featured: true, published: new Date('19/8/2014'), tags: ['селскостопанска литература, български класици']});
+            Library.create({name: 'Публичната библиотека на дядо Матейко', featured: false, published: new Date('19/8/2014'), tags: ['български класици']});
+            Library.create({name: 'Книжки ще има за всички от сърце', featured: false, published: new Date('19/8/2014'), tags: ['детска литература']});
+            Library.create({name: 'Библиотека за програмисти', featured: true, published: new Date('19/8/2014'), tags: ['техническа литература', 'програмиране']});
+            Library.create({name: 'Библиотека "For Dummies"', featured: true, published: new Date('19/8/2014'), tags: ['самообучители']});
+        }
+    });
+};
