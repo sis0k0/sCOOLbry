ede08569fcad26182aa15e3368a83a85f11e586a
sis0k0 <sis0k0@abv.bg>
2014-09-13 15:10:36 +0300

    Panels improved

--- public/scripts/controllers/admin-panel/adminPanelCtrl.js
+++ public/scripts/controllers/admin-panel/adminPanelCtrl.js
@@ -4,31 +4,7 @@ app.controller('AdminPanelCtrl', function($scope, $location, identity) {
 	$scope.identity = identity;
 	$scope.path = $location.path();
 
-	// Gets the current menu item
-	// using the current location path
-	$scope.getMenuItem = function(path) {
-		var menuItem = path.substr(1, path.length);
-
-		var indexOfNextSlash = menuItem.indexOf('/');
-		if(indexOfNextSlash === -1) {
-			return '';
-		}
-		menuItem = menuItem.substring(indexOfNextSlash+1, menuItem.length);
-
-		indexOfNextSlash = menuItem.indexOf('/');
-		if(indexOfNextSlash === -1) {
-			indexOfNextSlash = menuItem.length;
-		}
-		menuItem = menuItem.substring(0, indexOfNextSlash);
-
-		return menuItem;
-	}
-
-
-	$scope.menuItem = $scope.getMenuItem($scope.path);
-
 	$scope.menu = [
-
 		{
 			'name' : 'libraries',
 			links : {
@@ -57,4 +33,18 @@ app.controller('AdminPanelCtrl', function($scope, $location, identity) {
 		}
 	]
 
+	// Gets the current menu item
+	// using the current location path
+	$scope.getCurrentMenuItem = function(path) {
+		for(var i=0; i<$scope.menu.length; i++) {
+			for (var key in $scope.menu[i].links) {
+				if($scope.menu[i].links[key]===path){
+					return $scope.menu[i].name;
+				}
+			}
+		}
+		return '';
+	};
+
+	$scope.currentMenuItem = $scope.getCurrentMenuItem($scope.path);
 });
--- public/scripts/controllers/library-panel/libraryPanelCtrl.js
+++ public/scripts/controllers/library-panel/libraryPanelCtrl.js
@@ -4,28 +4,6 @@ app.controller('LibraryPanelCtrl', function($scope, identity, $location) {
     $scope.identity = identity;
 	$scope.path = $location.path();
 
-	$scope.getMenuItem = function(path) {
-		var menuItem = path.substr(1, path.length);
-
-		var indexOfNextSlash = menuItem.indexOf('/');
-		if(indexOfNextSlash === -1) {
-			return '';
-		}
-		menuItem = menuItem.substring(indexOfNextSlash+1, menuItem.length);
-
-		indexOfNextSlash = menuItem.indexOf('/');
-		if(indexOfNextSlash === -1) {
-			indexOfNextSlash = menuItem.length;
-		}
-		menuItem = menuItem.substring(0, indexOfNextSlash);
-
-		return menuItem;
-	}
-
-
-	$scope.menuItem = $scope.getMenuItem($scope.path);
-	$scope.singleMenuItem = $scope.menuItem.substring(1, $scope.menuItem.length);
-
 	$scope.menu = [
 
 		{
@@ -55,4 +33,21 @@ app.controller('LibraryPanelCtrl', function($scope, identity, $location) {
 			}
 		}
 	]
+
+
+	// Gets the current menu item
+	// using the current location path
+	$scope.getCurrentMenuItem = function(path) {
+		for(var i=0; i<$scope.menu.length; i++) {
+			for (var key in $scope.menu[i].links) {
+				if($scope.menu[i].links[key]===path){
+					return $scope.menu[i].name;
+				}
+			}
+		}
+		return '';
+	};
+
+	$scope.currentMenuItem = $scope.getCurrentMenuItem($scope.path);
+
 });
