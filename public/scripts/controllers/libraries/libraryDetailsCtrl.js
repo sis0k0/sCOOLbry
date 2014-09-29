f343d268625afe14d68aa019dbef883a92fec3a5
sis0k0 <sis0k0@abv.bg>
2014-09-29 22:32:38 +0300

    subscribe to lib

--- public/scripts/controllers/libraries/libraryDetailsCtrl.js
+++ public/scripts/controllers/libraries/libraryDetailsCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('LibraryDetailsPageCtrl', function($scope, $routeParams, cachedLibraries, LibBookResource, UserReadingResource, identity, $http) {
+app.controller('LibraryDetailsPageCtrl', function($scope, $routeParams, cachedLibraries, LibBookResource, UserReadingResource, identity, $http, auth, notifier, $location) {
     $scope.library = cachedLibraries.query().$promise.then(function(collection) {
         collection.forEach(function(library) {
             if (library._id === $routeParams.id) {
@@ -11,24 +11,38 @@ app.controller('LibraryDetailsPageCtrl', function($scope, $routeParams, cachedLi
     
     $scope.books = LibBookResource.query({id: $routeParams.id});
 
-
-    $scope.isMember = function() {
-		if(identity.currentUser===undefined) return false;
+    if(identity.currentUser===undefined) {
+		$scope.isMember = false;
+		$scope.isLoggedIn = false;
+	}else{
 		var responsePromise = $http.get('/api/library/member/'+$routeParams.id+'/'+identity.currentUser._id);
-	    responsePromise.success(function(data) {
-			console.log(data);
-            if(data==='true'){
-                return true;
-            }else{
-                return false;
-            }
-	    });           
-    };
+		responsePromise.success(function(data) {
+			if(data==='true'){
+				$scope.isMember = true;
+			}else{
+				$scope.isMember = false;
+			}
+		});      
+		
+		$scope.isLoggedIn = true;     
+	}
+	
     if(identity.currentUser===undefined) {
 		$scope.readings = [];
 	}else{
 		$scope.readings = UserReadingResource.query({ userID: identity.currentUser._id, libraryID: $routeParams.id });
 	}
+	
+	$scope.subscribeForLibrary = function() {
+		identity.currentUser.given = 0;
+		identity.currentUser.toReturn = 0;
+		identity.currentUser.userID = identity.currentUser._id;
+        auth.addUserToLibrary(identity.currentUser, $routeParams.id).then(function() {
+            notifier.success('You\'ve subscribed successfully!');
+            $location.path('/libraries');
+        }, function(reason){
+                notifier.error(reason);
+            });
+    };
 
-    console.log($scope.readings);
 });
