90f059d06302c45332269848dd8bc98b9acfd82a
sis0k0 <sis0k0@abv.bg>
2014-10-20 16:37:43 +0300

    booking schema

---
+++ server/models/Booking.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var bookingSchema = mongoose.Schema({
+    userID: String,
+    libraryID: String,
+    bookISBN: String,
+    bookDate: Date
+});
+
+var Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports.seedInitialReadings = function() {
+    Booking.find({}).exec(function(err, collection) {
+        if (err) {
+            console.log('Cannot find Bookings: ' + err);
+            return;
+		}
+		
+        if (collection.length === 0) {
+			//TODO: Insert some default data
+        }
+    });
+};
