a8af37a6516a919a23718da2bfd7f225b913e11f
sis0k0 <sis0k0@abv.bg>
2014-11-08 12:16:04 +0200

    Fixed design bug

--- START APPLICATION FROM HERE.bat
+++ START APPLICATION FROM HERE.bat
@@ -7,7 +7,7 @@ IF EXIST "C:\Program Files\nodejs\node.exe" (
 IF EXIST "C:\Program Files (x86)\MongoDB 2.6 Standard\bin\mongod.exe" (
 	START CMD /k "C:\Program Files (x86)\MongoDB 2.6 Standard\bin\mongod.exe"
 ) ELSE (
-	START CMD /k "C:\Program Files (x86)\MongoDB 2.6 Standard\bin\mongod.exe"
+	START CMD /k "C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe"
 )
 
 START http://localhost:3030
