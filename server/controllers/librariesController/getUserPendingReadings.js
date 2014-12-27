87aff90e742e02f8265cb655dd26bc5bdd4cf028
sis0k0 <sis0k0@abv.bg>
2014-12-27 17:26:57 +0200

    Get pendings finished

--- server/controllers/librariesController/getUserPendingReadings.js
+++ server/controllers/librariesController/getUserPendingReadings.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var Reading = require('mongoose').model('Reading'),
-	Booking = require('mongoose').model('Booking');
+	Booking = require('mongoose').model('Booking'),
+	Book    = require('mongoose').model('Book');
 
 module.exports = function(req, res) {
 
@@ -12,8 +13,25 @@ module.exports = function(req, res) {
 			console.log('Readings could not be loaded: ' + err);
 		}
 
-		history = history.concat(collection);
+		/*jshint loopfunc:true */
+		for(var i=0; i<collection.length; i++) {
 
+			Book.findOne({_id: collection[i].bookID}).exec(function(err, book) {
+				if (err) {
+					console.log('Book could not be loaded: ' + err);
+				} else {
+					var reading = new Object({});
+					reading.type = 'reading';
+					reading.book = book;
+					reading.end = collection[collection.length - history.length - 1].endDate;
+
+					history.push(reading);
+				}
+			});
+		}
+		/*jshint loopfunc:false */
+
+		var readingsCount = collection.length;
 		var now = new Date();
 		now = Date.now();
 
@@ -21,8 +39,29 @@ module.exports = function(req, res) {
 			if(err) {
 				console.log('Bookings could not be loaded: ' + err);
 			}
-			history = history.concat(collection);
-			res.send(history);
+
+			/*jshint loopfunc:true */
+			for(var i=0; i<collection.length; i++) {
+
+				Book.findOne({_id: collection[i].bookID}).exec(function(err, book) {
+					if (err) {
+						console.log('Book could not be loaded: ' + err);
+					} else {
+						var booking = new Object({});
+						booking.type = 'booking';
+						booking.book = book;
+						booking.end = collection[collection.length - history.length - 1 + readingsCount].bookDate;
+
+						history.push(booking);
+
+						if(book._id.toString()===collection[collection.length-1].bookID.toString()) {
+							res.send(history);
+						}
+					}
+				});
+			}
+			/*jshint loopfunc:false */
+
 		});
 
 	});
