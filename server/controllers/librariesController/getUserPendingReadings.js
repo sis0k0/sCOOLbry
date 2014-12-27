989255774cdf0301ef82ea84e9cfde18caed6d98
sis0k0 <sis0k0@abv.bg>
2014-12-27 19:00:41 +0200

    Pending history bug fix

--- server/controllers/librariesController/getUserPendingReadings.js
+++ server/controllers/librariesController/getUserPendingReadings.js
@@ -2,67 +2,76 @@
 
 var Reading = require('mongoose').model('Reading'),
 	Booking = require('mongoose').model('Booking'),
-	Book    = require('mongoose').model('Book');
+	Book    = require('mongoose').model('Book'),
+	User    = require('mongoose').model('User');
 
 module.exports = function(req, res) {
 
 	var history = [];
-
-	Reading.find({userID: req.params.userID, libraryID: req.params.libraryID, returnDate: undefined}).exec(function(err, collection) {
+	User.findOne({id: req.params.userID}).exec(function(err, user) {
 		if (err) {
-			console.log('Readings could not be loaded: ' + err);
-		}
+			res.send('User cannot be found');
+			
+		}else{
 
-		/*jshint loopfunc:true */
-		for(var i=0; i<collection.length; i++) {
-
-			Book.findOne({_id: collection[i].bookID}).exec(function(err, book) {
+			Reading.find({userID: user._id, libraryID: req.params.libraryID, returnDate: undefined}).exec(function(err, collection) {
 				if (err) {
-					console.log('Book could not be loaded: ' + err);
-				} else {
-					var reading = new Object({});
-					reading.type = 'reading';
-					reading.book = book;
-					reading.end = collection[collection.length - history.length - 1].endDate;
-
-					history.push(reading);
+					console.log('Readings could not be loaded: ' + err);
 				}
-			});
-		}
-		/*jshint loopfunc:false */
+				/*jshint loopfunc:true */
+				for(var i=0; i<collection.length; i++) {
 
-		var readingsCount = collection.length;
-		var now = new Date();
-		now = Date.now();
+					Book.findOne({_id: collection[i].bookID}).exec(function(err, book) {
+						if (err) {
+							console.log('Book could not be loaded: ' + err);
+						} else {
+							var reading = new Object({});
+							reading.type = 'reading';
+							reading.book = book;
+							reading.end = collection[collection.length - history.length - 1].endDate;
 
-		Booking.find({userID: req.params.userID, libraryID: req.params.libraryID, bookDate: {$gte: now }}).exec(function(err, collection) {
-			if(err) {
-				console.log('Bookings could not be loaded: ' + err);
-			}
-
-			/*jshint loopfunc:true */
-			for(var i=0; i<collection.length; i++) {
-
-				Book.findOne({_id: collection[i].bookID}).exec(function(err, book) {
-					if (err) {
-						console.log('Book could not be loaded: ' + err);
-					} else {
-						var booking = new Object({});
-						booking.type = 'booking';
-						booking.book = book;
-						booking.end = collection[collection.length - history.length - 1 + readingsCount].bookDate;
-
-						history.push(booking);
-
-						if(book._id.toString()===collection[collection.length-1].bookID.toString()) {
-							res.send(history);
+							history.push(reading);
 						}
+					});
+				}
+				/*jshint loopfunc:false */
+
+				var readingsCount = collection.length;
+				var now = new Date();
+				now = Date.now();
+
+				Booking.find({userID: user._id, libraryID: req.params.libraryID, bookDate: {$gte: now }}).exec(function(err, collection) {
+					if(err) {
+						console.log('Bookings could not be loaded: ' + err);
 					}
+
+					/*jshint loopfunc:true */
+					for(var i=0; i<collection.length; i++) {
+
+						Book.findOne({_id: collection[i].bookID}).exec(function(err, book) {
+							if (err) {
+								console.log('Book could not be loaded: ' + err);
+							} else {
+								var booking = new Object({});
+								booking.type = 'booking';
+								booking.book = book;
+								booking.end = collection[collection.length - history.length - 1 + readingsCount].bookDate;
+
+								history.push(booking);
+
+								if(book._id.toString()===collection[collection.length-1].bookID.toString()) {
+									res.send(history);
+								}
+							}
+						});
+					}
+					/*jshint loopfunc:false */
+
 				});
-			}
-			/*jshint loopfunc:false */
 
-		});
+			});
 
+		}
 	});
+
 };
