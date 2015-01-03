0a8e4c5ae04a121f4b009916cd23bc2eaee35f39
sis0k0 <sis0k0@abv.bg>
2015-01-03 14:03:39 +0200

    onFocus directive
    Autofocus doesn't work well on SPA, so I added onFocus directive.

---
+++ public/scripts/directives/onFocus.js
@@ -0,0 +1,10 @@
+'use strict';
+
+app.directive('onFocus', function () {
+    return {
+      restrict: 'A',
+      link: function (scope, element) {
+      	element.focus();
+      }
+    };
+  });
