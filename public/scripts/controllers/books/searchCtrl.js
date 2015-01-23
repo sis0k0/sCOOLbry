2480302ec8896933844319046b34862ffe7f13d0
sis0k0 <sis0k0@abv.bg>
2015-01-24 01:28:55 +0200

    search functionality without keywords. TODO: better design

--- public/scripts/controllers/books/searchCtrl.js
+++ public/scripts/controllers/books/searchCtrl.js
@@ -2,12 +2,22 @@
 
 app.controller('SearchCtrl', function($scope, $location, $routeParams, BookSearchResource) {
     
+    $scope.showResults = false;
+
     $scope.search = function(phrase) {
-        if(typeof phrase === 'undefined' || phrase===' ') {
+        console.log(phrase);
+        if(typeof phrase === 'undefined' || phrase===' ' || phrase==='') {
             $scope.results = undefined;
+            $scope.showResults = false;
         }else{
             $scope.phrase = phrase;
             $scope.results = BookSearchResource.query({phrase: phrase, limit: 4});
+            console.log($scope.results.length);
+            if($scope.results.length===0){
+                $scope.showResults = false;
+            }else{
+                $scope.showResults = true;
+            }
         }
     };
 
@@ -18,7 +28,11 @@ app.controller('SearchCtrl', function($scope, $location, $routeParams, BookSearc
     if($routeParams.phrase!==undefined) {
         $scope.phrase = $routeParams.phrase;
         $scope.books = BookSearchResource.query({phrase: $routeParams.phrase, limit: 10});
-
-        console.log($scope.books);
     }
+
+    $scope.hideResults = function() {
+        $scope.results = undefined;
+    };
+
+
 });
