362194d380cea1a878998c43c763ef8b793409c4
sis0k0 <sis0k0@abv.bg>
2015-02-05 09:35:29 +0000

    google+ routes

--- server/config/routes/api/users.js
+++ server/config/routes/api/users.js
@@ -49,6 +49,14 @@ module.exports = function(app, config) {
             successRedirect : '/profile',
             failureRedirect : '/'
         }));
+    
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
+
 
     router.post('/images', controllers.users.uploadAvatar);
 
