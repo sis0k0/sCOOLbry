35ddaf6eb81e3584603887b0afbe635c7ade3841
sis0k0 <sis0k0@abv.bg>
2015-04-14 09:08:51 +0300

    Updated package json

---
+++ public/other/bootstrap-menu-fix.js
@@ -0,0 +1,13 @@
+'use strict';
+
+jQuery(document).on('click','#admin-menu.in,#library-menu.in',function(e) {
+    if( jQuery(e.target).is('a') && jQuery(e.target).attr('data-toggle')!=='tab') {
+        jQuery(this).collapse('hide');
+    }
+});
+
+jQuery(document).on('click','#site-navigation.in',function(e) {
+    if( jQuery(e.target).is('a')) {
+        jQuery(this).collapse('hide');
+    }
+});
