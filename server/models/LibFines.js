49171fbe9b8618abd1dd46b459260d6eb1bb4075
sis0k0 <sis0k0@abv.bg>
2015-04-20 01:33:00 +0300

    switched to paypal-rest-sdk

---
+++ server/models/LibFines.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    ObjectId = require('mongoose').Schema.ObjectId;
+
+var libFinesSchema = mongoose.Schema({
+    userID: {
+        type: ObjectId,
+        ref: 'User',
+        required: '{PATH} is required'
+    },
+    libraryID: {
+        type: ObjectId,
+        ref: 'Library',
+        required: '{PATH} is required'
+    },
+    username: String,
+    reason: String,
+    added: {
+        type: Date,
+        default: Date.now
+    },
+    paid: {
+        type: Date
+    }
+});
+
+var LibFines = mongoose.model('LibFines', libFinesSchema);
+
+module.exports.seedInitialLibFines = function() {
+    LibFines.find({}).exec(function(err, collection) {
+        if (err) {
+            console.log('Cannot find fines: ' + err);
+            return;
+        }
+        
+        if (collection.length === 0) {
+            //TODO: Create a few fines
+        }
+    });
+};
