4df3df87d6454df067fca99a2de8fe26841ce80d
sis0k0 <sis0k0@abv.bg>
2015-04-22 21:00:32 +0300

    File missed

--- public/scripts/controllers/account/notificationsCtrl.js
+++ public/scripts/controllers/account/notificationsCtrl.js
@@ -7,13 +7,49 @@ app.controller('NotificationsCtrl', function($scope, $http, Socket, Notification
     $scope.user = identity.currentUser;
 
     // Get user notifications function
-    $scope.loadNotifications = function() {
-        NotificationService.getForUser($scope.user).
-        then(function(notifications) {
+    var loadNotifications = function(callback) {
+        NotificationService.getForUser($scope.user)
+        .then(function(notifications) {
             notifications.reverse();
-            $scope.notifications = notifications;
+            return callback(notifications);
         });
     };
+
+    /* jshint unused:false */
+    /* global Notification: false */
+    var triggerDesktopNotification = function(notificationMessage) {
+        // Let's check if the browser supports notifications
+        if (!('Notification' in window)) {
+            alert('This browser does not support desktop notification');
+        }
+
+        // Let's check if the user is okay to get some notification
+        else if (Notification.permission === 'granted') {
+            // If it's okay let's create a notification
+            var notification = new Notification('New sCOOLbry notification!', {
+                body: notificationMessage.replace(/(<([^>]+)>)/ig, ''), // Remove the html tags
+                icon: '../../dist/images/favicon.png'
+            });
+        }
+
+        // Otherwise, we need to ask the user for permission
+        else if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+                // If the user is okay, let's create a notification
+                if (permission === 'granted') {
+                    var notification = new Notification('New sCOOLbry notification!', {
+                        body: notificationMessage.replace(/(<([^>]+)>)/ig, ''), // Remove the html tags
+                        icon: '../../dist/images/favicon.png'
+                    });
+                }
+            });
+        }
+
+        // At last, if the user already denied any notification, and you 
+        // want to be respectful there is no need to bother them any more.
+    };
+    /* global Notification: true */
+    /* jshint unused:true */
 
     // Update notifications (seen->true)
     $scope.markAsSeen = function(notification) {
@@ -23,11 +59,13 @@ app.controller('NotificationsCtrl', function($scope, $http, Socket, Notification
 
     if(!!$scope.user) {
 
-        $scope.notifications = [];
-        $scope.loadNotifications();
+        loadNotifications(function(notifications) {
+            $scope.notifications = notifications;
+        });
 
         // Listen for new notifications
         Socket.on($scope.user._id + ' notification added', function(notification) {
+            triggerDesktopNotification(notification.message);
             $scope.notifications.unshift(notification);
         });
 
