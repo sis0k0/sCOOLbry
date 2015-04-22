78ce36f4d29061cc6383752bde1b39cbdafb424b
sis0k0 <sis0k0@abv.bg>
2015-04-22 21:00:23 +0300

    Desktop notifications

--- server/controllers/notificationsController/getUserNotifications.js
+++ server/controllers/notificationsController/getUserNotifications.js
@@ -6,7 +6,6 @@ module.exports = function(req, res) {
     Notification
     .find({userID: req.params.id})
     .exec(function(err, collection) {
-        console.log(collection);
         if (err) {
             console.log('Cannot load notifications: ' + err);
             res.status(503).send('Cannot connect to database');
