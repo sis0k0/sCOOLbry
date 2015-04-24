782460dc68b84ac55bfc02f817769ba700bdc32b
sis0k0 <sis0k0@abv.bg>
2015-04-24 14:02:29 +0300

    Added new test database

--- server/config/config.js
+++ server/config/config.js
@@ -31,6 +31,10 @@ module.exports = {
         // db: 'mongodb://admin:test@ds029541.mongolab.com:29541/scoolbry2',
         db: 'mongodb://admin:test@ds053109.mongolab.com:53109/scoolbry',
         // db: 'mongodb://admin:test@ds043971.mongolab.com:43971/scoolbry-test',
+
+
+        // DEMO
+        // db: 'mongodb://admin:test@ds045097.mongolab.com:45097/scoolbry-testfordemo',
         // db: 'mongodb://admin:test@dbh13.mongolab.com:27137/scoolbry-demo',
         port: process.env.PORT || 3030,
         facebook: {
