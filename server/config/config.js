b8e292b978e9f1905ec10d2d12c1e2ae6600d00f
sis0k0 <sis0k0@abv.bg>
2015-04-24 13:16:58 +0300

    Database switch

--- server/config/config.js
+++ server/config/config.js
@@ -29,9 +29,9 @@ module.exports = {
     production: {
         rootPath: rootPath,
         // db: 'mongodb://admin:test@ds029541.mongolab.com:29541/scoolbry2',
-        // db: 'mongodb://admin:test@ds053109.mongolab.com:53109/scoolbry',
+        db: 'mongodb://admin:test@ds053109.mongolab.com:53109/scoolbry',
         // db: 'mongodb://admin:test@ds043971.mongolab.com:43971/scoolbry-test',
-        db: 'mongodb://admin:test@dbh13.mongolab.com:27137/scoolbry-demo',
+        // db: 'mongodb://admin:test@dbh13.mongolab.com:27137/scoolbry-demo',
         port: process.env.PORT || 3030,
         facebook: {
             clientID: '1511950165721139',
