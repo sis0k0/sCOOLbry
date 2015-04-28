7cb752b7f79cae21dc4bae18f43faed3b216276c
sis0k0 <sis0k0@abv.bg>
2015-04-28 16:06:56 +0300

    Download ebook cross domain

--- server/controllers/booksController/downloadEbook.js
+++ server/controllers/booksController/downloadEbook.js
@@ -2,6 +2,9 @@
 
 module.exports = function(req, res) {
 
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
     res.download(req.query.path, 'book.epub',
         function(err) {
             console.log(err);
